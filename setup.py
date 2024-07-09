6d9564450bac34344633e69ca13cabc7b9f4d1d5
Gabriel Miranda <gabriel.oliveira@gomind.com.br>
2024-07-09 17:44:09 -0300

    fix: fix file name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="gomind_sqlite-to-excel",
+    name="gomind_sqlite_to_excel",
     python_requires=">=3.6",
     version="0.0.1",
     description="GoMind sqlite to excel functions",
@@ -9,7 +9,7 @@ setup(
     author="JeffersonCarvalhoGD",
     author_email="jefferson.carvalho@grupodomini.com",
     license="unlicense",
-    packages=["gomind_sqlite-to-excel"],
+    packages=["gomind_sqlite_to_excel"],
     zip_safe=False,
     install_requires=[],
 )
